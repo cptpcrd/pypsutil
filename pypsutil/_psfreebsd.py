26f7fbd185ff29b63dabd4f7900c9fbcef155bef
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-08-04 11:11:01 -0400

    _psfreebsd: Fix proc_cwd()

--- pypsutil/_psfreebsd.py
+++ pypsutil/_psfreebsd.py
@@ -40,6 +40,8 @@ KI_NGROUPS = 16
 LOGNAMELEN = 17
 LOGINCLASSLEN = 17
 
+PATH_MAX = 1024
+
 KI_CRF_GRP_OVERFLOW = 0x80000000
 
 gid_t = ctypes.c_uint32  # pylint: disable=invalid-name
@@ -64,6 +66,19 @@ else:
     time_t = ctypes.c_int64  # type: ignore
 
 suseconds_t = ctypes.c_long
+
+sa_family_t = ctypes.c_uint8
+
+_SS_MAXSIZE = 128
+_SS_ALIGNSIZE = ctypes.sizeof(ctypes.c_int64)
+_SS_PAD1SIZE = _SS_ALIGNSIZE - ctypes.sizeof(ctypes.c_ubyte) - ctypes.sizeof(sa_family_t)
+_SS_PAD2SIZE = (
+    _SS_MAXSIZE
+    - ctypes.sizeof(ctypes.c_ubyte)
+    - ctypes.sizeof(sa_family_t)
+    - _SS_PAD1SIZE
+    - _SS_ALIGNSIZE
+)
 
 
 class Rlimit(ctypes.Structure):
@@ -227,6 +242,131 @@ class KinfoProc(ctypes.Structure):
         return list(self.ki_groups[: self.ki_ngroups])
 
 
+class SockaddrStorage(ctypes.Structure):
+    _fields = [
+        ("ss_len", ctypes.c_ubyte),
+        ("ss_family", sa_family_t),
+        ("ss_pad1", (ctypes.c_char * _SS_PAD1SIZE)),
+        ("ss_align", ctypes.c_int64),
+        ("ss_pad2", (ctypes.c_char * _SS_PAD2SIZE)),
+    ]
+
+
+class KinfoFile11(ctypes.Structure):
+    _fields_ = [
+        ("kf_vnode_type", ctypes.c_int),
+        ("kf_sock_domain", ctypes.c_int),
+        ("kf_sock_type", ctypes.c_int),
+        ("kf_sock_protocol", ctypes.c_int),
+        ("kf_sa_local", SockaddrStorage),
+        ("kf_sa_peer", SockaddrStorage),
+    ]
+
+
+class KinfoFileSock(ctypes.Structure):
+    _fields_ = [
+        ("kf_sock_sendq", ctypes.c_uint32),
+        ("kf_sock_domain0", ctypes.c_int),
+        ("kf_sock_type0", ctypes.c_int),
+        ("kf_sock_protocol0", ctypes.c_int),
+        ("kf_sa_local", SockaddrStorage),
+        ("kf_sa_peer", SockaddrStorage),
+        ("kf_sock_pcb", ctypes.c_uint64),
+        ("kf_sock_inpcb", ctypes.c_uint64),
+        ("kf_sock_unpconn", ctypes.c_uint64),
+        ("kf_sock_snd_sb_state", ctypes.c_uint16),
+        ("kf_sock_rcv_sb_state", ctypes.c_uint16),
+        ("kf_sock_recvq", ctypes.c_uint32),
+    ]
+
+
+class KinfoFileFile(ctypes.Structure):
+    _fields_ = [
+        ("kf_file_type", ctypes.c_int),
+        ("kf_spareint", (ctypes.c_int * 3)),
+        ("kf_spareint64", (ctypes.c_uint64 * 30)),
+        ("kf_file_fsid", ctypes.c_uint64),
+        ("kf_file_rdev", ctypes.c_uint64),
+        ("kf_file_fileid", ctypes.c_uint64),
+        ("kf_file_size", ctypes.c_uint64),
+        ("kf_file_fsid_freebsd11", ctypes.c_uint32),
+        ("kf_file_rdev_freebsd11", ctypes.c_uint32),
+        ("kf_file_mode", ctypes.c_uint16),
+        ("kf_file_pad0", ctypes.c_uint16),
+        ("kf_file_pad1", ctypes.c_uint32),
+    ]
+
+
+class KinfoFileSem(ctypes.Structure):
+    _fields_ = [
+        ("kf_spareint", (ctypes.c_uint32 * 4)),
+        ("kf_spareint64", (ctypes.c_uint64 * 32)),
+        ("kf_sem_value", ctypes.c_uint32),
+        ("kf_sem_mode", ctypes.c_uint16),
+    ]
+
+
+class KinfoFilePipe(ctypes.Structure):
+    _fields_ = [
+        ("kf_spareint", (ctypes.c_uint32 * 4)),
+        ("kf_spareint64", (ctypes.c_uint64 * 32)),
+        ("kf_pipe_addr", ctypes.c_uint64),
+        ("kf_pipe_peer", ctypes.c_uint64),
+        ("kf_pipe_buffer_cnt", ctypes.c_uint32),
+        ("kf_pts_pad0", (ctypes.c_uint32 * 3)),
+    ]
+
+
+class KinfoFilePts(ctypes.Structure):
+    _fields_ = [
+        ("kf_spareint", (ctypes.c_uint32 * 4)),
+        ("kf_spareint64", (ctypes.c_uint64 * 32)),
+        ("kf_pts_dev_freebsd11", ctypes.c_uint32),
+        ("kf_pts_pad0", ctypes.c_uint32),
+        ("kf_pts_dev", ctypes.c_uint64),
+        ("kf_pts_pad1", (ctypes.c_uint32 * 4)),
+    ]
+
+
+class KinfoFileProc(ctypes.Structure):
+    _fields_ = [
+        ("kf_spareint", (ctypes.c_uint32 * 4)),
+        ("kf_spareint64", (ctypes.c_uint64 * 32)),
+        ("kf_pid", _ffi.pid_t),
+    ]
+
+
+class KinfoFileUn(ctypes.Union):
+    _fields_ = [
+        ("kf_freebsd11", KinfoFile11),
+        ("kf_sock", KinfoFileSock),
+        ("kf_file", KinfoFileFile),
+        ("kf_sem", KinfoFileSem),
+        ("kf_pipe", KinfoFilePipe),
+        ("kf_pts", KinfoFilePts),
+        ("kf_proc", KinfoFileProc),
+    ]
+
+
+class KinfoFile(ctypes.Structure):
+    _fields_ = [
+        ("kf_structsize", ctypes.c_int),
+        ("kf_type", ctypes.c_int),
+        ("kf_fd", ctypes.c_int),
+        ("kf_ref_count", ctypes.c_int),
+        ("kf_flags", ctypes.c_int),
+        ("kf_pad0", ctypes.c_int),
+        ("kf_offset", ctypes.c_int),
+        ("kf_un", KinfoFileUn),
+        ("kf_status", ctypes.c_uint16),
+        ("kf_pad1", ctypes.c_uint16),
+        ("_kfispare0", ctypes.c_int),
+        ("kf_status", ctypes.c_uint16),
+        ("kf_status", ctypes.c_uint16),
+        ("kf_path", (ctypes.c_char * PATH_MAX)),
+    ]
+
+
 def _get_kinfo_proc_pid(pid: int) -> KinfoProc:
     proc_info = KinfoProc()
 
@@ -322,9 +462,9 @@ def proc_getgroups(proc: "Process") -> List[int]:
 
 
 def proc_cwd(proc: "Process") -> str:
-    return _bsd.sysctl_bytes_retry(
-        [CTL_KERN, KERN_PROC, KERN_PROC_CWD, proc.pid], None, trim_nul=True
-    ).decode()
+    cwd_info = KinfoFile()
+    _bsd.sysctl([CTL_KERN, KERN_PROC, KERN_PROC_CWD, proc.pid], None, cwd_info)
+    return cast(str, cwd_info.kf_path.decode())
 
 
 def proc_exe(proc: "Process") -> str:
