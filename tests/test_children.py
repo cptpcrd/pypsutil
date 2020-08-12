5433eb3bab4926118e250c931a29ff2479d79207
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-08-12 16:52:55 -0400

    tests/test_children: Refactor Process.children() tests
    This should fix some strange race conditions I've been seeing.

--- tests/test_children.py
+++ tests/test_children.py
@@ -8,31 +8,21 @@ from .util import fork_proc
 
 
 def test_children() -> None:
-    cur_proc = pypsutil.Process()
-    child_proc = fork_proc(lambda: sys.exit(0))
-
-    assert cur_proc.children() == [child_proc]
-
-    os.waitpid(child_proc.pid, 0)
-
-
-def test_children_recursive_simple() -> None:
-    cur_proc = pypsutil.Process()
-    child_proc = fork_proc(lambda: sys.exit(0))
-
-    assert cur_proc.children(recursive=True) == [child_proc]
-
-    os.waitpid(child_proc.pid, 0)
-
-
-def test_children_recursive_grancdhildren() -> None:
     def child_callback() -> None:
         fork_proc(lambda: sys.exit(0))
         # Don't reap our child (the grandchild of the testing process), so it will still show up
         time.sleep(100)
 
     cur_proc = pypsutil.Process()
+
+    # We have no children
+    assert cur_proc.children() == []
+    assert cur_proc.children(recursive=True) == []
+
     child_proc = fork_proc(child_callback)
+
+    # Now we have only one
+    assert cur_proc.children() == [child_proc]
 
     # Wait for the child to fork()
     while True:
@@ -42,10 +32,15 @@ def test_children_recursive_grancdhildren() -> None:
 
         time.sleep(0.01)
 
+    # Our child only has one child
     assert len(grandchildren) == 1
 
+    # The child process has no grandchildren, so children(recursive=True) should report the same
+    # as children()
+    assert child_proc.children(recursive=True) == grandchildren
+
     # Make sure the grandchild gets pulled in by recursive=True
-    assert cur_proc.children(recursive=True) == [child_proc, *grandchildren]
+    assert set(cur_proc.children(recursive=True)) == {child_proc, *grandchildren}
 
     # Kill the child and wait for it.
     # The grandchild should be reparented to (and reaped by) PID 1.
