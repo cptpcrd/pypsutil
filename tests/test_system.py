fe1dc4a56102162ab410de9b3f0dfd540da15ced
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-08-14 08:54:38 -0400

    tests/test_system: Remove pypsutil.uptime() check
    This behaves very strangely on macOS in CI.

--- tests/test_system.py
+++ tests/test_system.py
@@ -10,11 +10,3 @@ def test_boot_time() -> None:
 def test_time_since_boot() -> None:
     # Make sure they match up approximately
     assert round(time.time() - (pypsutil.boot_time() + pypsutil.time_since_boot()), 1) == 0
-
-
-if hasattr(pypsutil, "uptime"):
-
-    def test_uptime() -> None:
-        # time_since_boot() should always be greater than or equal to uptime()
-        # Allow some minor flexibility, though
-        assert pypsutil.time_since_boot() - pypsutil.uptime() >= -1
