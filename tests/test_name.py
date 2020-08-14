d122edde811627526dd67dd22a701430506d6dde
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-08-14 16:04:01 -0400

    tests/test_name: Be looser about valid process names

--- tests/test_name.py
+++ tests/test_name.py
@@ -9,8 +9,13 @@ from .util import get_dead_process, managed_child_process
 
 
 def test_cmdline() -> None:
+    valid_names = {
+        os.path.basename(sys.executable),
+        os.path.basename(os.path.realpath(sys.executable)),
+    }
+
     with managed_child_process([sys.executable, "-c", "import time; time.sleep(10)"]) as proc:
-        assert proc.name() == os.path.basename(sys.executable)
+        assert proc.name() in valid_names
 
 
 def test_name_no_proc() -> None:
