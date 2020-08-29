ba40db38402a8d1f763eb0e271a85b7c12d3e499
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-08-28 20:57:53 -0400

    _bsd: Add sysctlbyname() helper

--- pypsutil/_bsd.py
+++ pypsutil/_bsd.py
@@ -16,6 +16,15 @@ libc.sysctl.argtypes = (
 )
 libc.sysctl.restype = ctypes.c_int
 
+libc.sysctlbyname.argtypes = (
+    ctypes.c_char_p,
+    ctypes.c_void_p,
+    ctypes.POINTER(ctypes.c_size_t),
+    ctypes.c_void_p,
+    ctypes.c_size_t,
+)
+libc.sysctlbyname.restype = ctypes.c_int
+
 
 def sysctl(
     mib: Collection[int],
@@ -42,6 +51,34 @@ def sysctl(
         raw_old = ctypes.byref(old)
 
     if libc.sysctl(raw_mib, len(mib), raw_old, ctypes.byref(old_size), raw_new, new_size) < 0:
+        raise _ffi.build_oserror(ctypes.get_errno())
+
+    return old_size.value
+
+
+def sysctlbyname(
+    name: str,
+    new: Union[None, bytes, ctypes.Array, ctypes.Structure],  # type: ignore
+    old: Union[None, ctypes.Array, ctypes.Structure],  # type: ignore
+) -> int:
+    if new is None:
+        new_size = 0
+        raw_new = None
+    elif isinstance(new, bytes):
+        new_size = len(new)
+        raw_new = ctypes.byref(ctypes.create_string_buffer(new))
+    else:
+        new_size = ctypes.sizeof(new)
+        raw_new = ctypes.byref(new)
+
+    if old is None:
+        old_size = ctypes.c_size_t(0)
+        raw_old = None
+    else:
+        old_size = ctypes.c_size_t(ctypes.sizeof(old))
+        raw_old = ctypes.byref(old)
+
+    if libc.sysctlbyname(name.encode(), raw_old, ctypes.byref(old_size), raw_new, new_size) < 0:
         raise _ffi.build_oserror(ctypes.get_errno())
 
     return old_size.value
