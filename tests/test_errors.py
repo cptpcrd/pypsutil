3e73379de6bf3afffa7f613680019643aa5672a5
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-08-30 14:50:06 -0400

    tests/test_errors: Test behavior of TimeoutExpired error

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -23,3 +23,19 @@ def test_accessdenied() -> None:
     assert ex.pid == 12
     assert str(ex) == "pypsutil.AccessDenied (pid=12)"
     assert repr(ex) == "pypsutil.AccessDenied(pid=12)"
+
+
+def test_timeoutexpired() -> None:
+    ex1 = pypsutil.TimeoutExpired(1.2)
+
+    assert ex1.seconds == 1.2
+    assert ex1.pid is None
+    assert str(ex1) == "pypsutil.TimeoutExpired: timeout after 1.2 seconds"
+    assert repr(ex1) == "pypsutil.TimeoutExpired(1.2, pid=None)"
+
+    ex2 = pypsutil.TimeoutExpired(3, 10)
+
+    assert ex2.seconds == 3
+    assert ex2.pid == 10
+    assert str(ex2) == "pypsutil.TimeoutExpired: timeout after 3 seconds (pid=10)"
+    assert repr(ex2) == "pypsutil.TimeoutExpired(3, pid=10)"
