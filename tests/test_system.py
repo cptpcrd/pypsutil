d9e39d40f41d08e1e0a2b0ab5758f8d9ae25ff10
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-08-31 09:38:40 -0400

    tests/test_system: Add uptime() test back
    It *should* now be fixed on macOS.

--- tests/test_system.py
+++ tests/test_system.py
@@ -13,6 +13,17 @@ def test_time_since_boot() -> None:
     assert round(time.time() - (pypsutil.boot_time() + pypsutil.time_since_boot()), 1) == 0
 
 
+if hasattr(pypsutil, "uptime"):
+
+    def test_uptime() -> None:
+        # time_since_boot() should always be greater than or equal to uptime()
+        # Allow some minor flexibility, though
+        assert (
+            pypsutil.time_since_boot() + 0.1
+            >= pypsutil.uptime()  # type: ignore  # pylint: disable=no-member
+        )
+
+
 def test_physical_cpu_count() -> None:
     logical_count = os.cpu_count()
     phys_count = pypsutil.physical_cpu_count()
