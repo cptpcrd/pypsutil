cd344820e357758b602e4dd8f0dfc7b6a60884e4
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-09-06 22:22:45 -0400

    tests/test_files: Add open_files() tests

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import sys
 
@@ -26,3 +27,51 @@ if hasattr(pypsutil.Process, "num_fds"):
 
         with pytest.raises(pypsutil.NoSuchProcess):
             proc.num_fds()
+
+
+if hasattr(pypsutil.Process, "open_files"):
+
+    def test_open_files_empty() -> None:
+        with managed_child_process2(
+            [sys.executable, "-c", "import time; print('a', flush=True); time.sleep(10)"],
+            close_fds=True,
+            stdout=subprocess.PIPE,
+            bufsize=0,
+        ) as proc:
+            assert proc.stdout is not None
+            assert proc.stdout.read(1) == b"a"
+
+            assert proc.open_files() == []
+
+    def test_open_files_2() -> None:
+        with managed_child_process2(
+            [
+                sys.executable,
+                "-c",
+                """
+import os, sys, time
+os.open("/", os.O_RDONLY)
+os.open(sys.executable, os.O_RDONLY)
+print('a', flush=True)
+time.sleep(10)
+""",
+            ],
+            close_fds=True,
+            stdout=subprocess.PIPE,
+            bufsize=0,
+        ) as proc:
+            assert proc.stdout is not None
+            assert proc.stdout.read(1) == b"a"
+
+            open_files = proc.open_files()
+
+            assert len(open_files) == 1
+            assert open_files[0].fd == 4
+            if open_files[0].path:
+                assert os.path.samefile(open_files[0].path, sys.executable)
+
+    def test_open_files_no_proc() -> None:
+        proc = get_dead_process()
+
+        with pytest.raises(pypsutil.NoSuchProcess):
+            proc.open_files()
