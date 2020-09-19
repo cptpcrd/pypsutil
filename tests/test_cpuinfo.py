f2adfc1879c1b972a249ff654f9ec354fd64b2ff
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-09-19 12:06:53 -0400

    tests/test_cpuinfo: Test cpu_stats()/cpu_times()/percpu_times() on Linux

--- tests/test_cpuinfo.py
+++ tests/test_cpuinfo.py
@@ -1,5 +1,8 @@
 # pylint: disable=no-member
+import os
 import pathlib
+
+import pytest
 
 import pypsutil
 
@@ -88,3 +91,120 @@ def test_cpu_freq_none(tmp_path: pathlib.Path) -> None:
         assert pypsutil.percpu_freq() == []  # type: ignore
 
         assert pypsutil.cpu_freq() is None  # type: ignore
+
+
+@linux_only  # type: ignore
+def test_cpu_stats(tmp_path: pathlib.Path) -> None:
+    with open(tmp_path / "stat", "w") as file:
+        file.write(
+            """cpu 0 0 0 0 0 0 0 0 0 0
+cpu0 0 0 0 0 0 0 0 0 0 0
+page 0 0
+swap 0 0
+intr 1234 10 24 425
+ctxt 455
+btime 0
+processes 100
+procs_running 1
+procs_blocked 0
+softirq 900 0 500 400
+"""
+        )
+
+    with replace_info_directories(procfs=str(tmp_path)):
+        assert pypsutil.cpu_stats() == pypsutil.CPUStats(  # type: ignore
+            ctx_switches=455,
+            interrupts=1234,
+            soft_interrupts=900,
+            syscalls=0,
+        )
+
+
+CLK_TCK = os.sysconf(os.sysconf_names["SC_CLK_TCK"])
+
+
+@linux_only  # type: ignore
+def test_cpu_times(tmp_path: pathlib.Path) -> None:
+    with open(tmp_path / "stat", "w") as file:
+        file.write(
+            """cpu {}
+cpu0 0 0 0 0 0 0 0 0 0 0
+page 0 0
+swap 0 0
+""".format(
+                # This has an extra field to test how pypsutil handles that
+                " ".join(str(val * CLK_TCK) for val in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+            )
+        )
+
+    with replace_info_directories(procfs=str(tmp_path)):
+        assert pypsutil.cpu_times() == pypsutil.CPUTimes(  # type: ignore
+            user=1,
+            nice=2,
+            system=3,
+            idle=4,
+            iowait=5,
+            irq=6,
+            softirq=7,
+            steal=8,
+            guest=9,
+            guest_nice=10,
+        )
+
+
+@linux_only  # type: ignore
+def test_cpu_times_empty(tmp_path: pathlib.Path) -> None:
+    # No "cpu" or "cpuN" entries
+    with open(tmp_path / "stat", "w") as file:
+        file.write("page 0 0\nswap 0 0\n")
+
+    with replace_info_directories(procfs=str(tmp_path)):
+        with pytest.raises(RuntimeError):
+            pypsutil.cpu_times()  # type: ignore
+
+        assert pypsutil.percpu_times() == []  # type: ignore
+
+
+@linux_only  # type: ignore
+def test_percpu_times(tmp_path: pathlib.Path) -> None:
+    with open(tmp_path / "stat", "w") as file:
+        file.write(
+            """cpu 0 0 0 0 0 0 0 0 0 0
+cpu0 {}
+cpu1 {}
+page 0 0
+swap 0 0
+""".format(
+                " ".join(str(val * CLK_TCK) for val in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+                # This has an extra field to test how pypsutil handles that
+                " ".join(str(val * CLK_TCK) for val in [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
+            )
+        )
+
+    with replace_info_directories(procfs=str(tmp_path)):
+        assert pypsutil.percpu_times() == [  # type: ignore
+            pypsutil.CPUTimes(  # type: ignore
+                user=1,
+                nice=2,
+                system=3,
+                idle=4,
+                iowait=5,
+                irq=6,
+                softirq=7,
+                steal=8,
+                guest=9,
+                guest_nice=10,
+            ),
+            pypsutil.CPUTimes(  # type: ignore
+                user=2,
+                nice=3,
+                system=4,
+                idle=5,
+                iowait=6,
+                irq=7,
+                softirq=8,
+                steal=9,
+                guest=10,
+                guest_nice=11,
+            ),
+        ]
